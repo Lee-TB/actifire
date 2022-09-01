6b34ba18c86bfe178fc4712e131da01c4ae09294
Lee TB <tblyduc2412@gmail.com>
2022-09-01 14:11:45 +0700

    feat: add antd breadcrumbs with react router dom

--- src/Layouts/MainLayout/Content.jsx
+++ src/Layouts/MainLayout/Content.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Layout, Breadcrumb } from 'antd';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 
 const { Content: AntContent } = Layout;
 
 function Content() {
+  const location = useLocation();
+  const pathSnippets = location.pathname.split('/').filter((i) => i);
+  const breadcrumbItems = pathSnippets.map((_, index) => {
+    const url = `/${pathSnippets.slice(0, index + 1).join('/')}`;
+    return (
+      <Breadcrumb.Item>
+        <Link to={url}>{_}</Link>
+      </Breadcrumb.Item>
+    );
+  });
+
   return (
     <AntContent style={{ minHeight: '100vh', padding: '0 50px' }}>
       <Breadcrumb
@@ -13,9 +24,7 @@ function Content() {
           margin: '16px 0',
         }}
       >
-        <Breadcrumb.Item>Your rooms</Breadcrumb.Item>
-        <Breadcrumb.Item>Hoat dong cong dong 2020</Breadcrumb.Item>
-        <Breadcrumb.Item>members</Breadcrumb.Item>
+        {breadcrumbItems}
       </Breadcrumb>
       <div style={{ minHeight: '280px', padding: '24px', background: '#fff' }}>
         <Outlet />
