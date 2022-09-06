46044a96be716cdf7832bc30f8033b24f3600979
Lee TB <tblyduc2412@gmail.com>
2022-09-06 15:25:04 +0700

    feat: Room Activities UI

---
+++ src/features/room/RoomActivities/RoomActivities.style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const ModalGroupStyled = styled.div`
+  margin-bottom: 8px;
+`;
+
+const TableContainerStyled = styled.div`
+  border: 1px solid #ccc;
+`;
+
+export { ModalGroupStyled, TableContainerStyled };
