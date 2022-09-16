70084e04cc60ec3854af39aaadea77dfe29fe7e8
Lee TB <tblyduc2412@gmail.com>
2022-09-16 20:06:14 +0700

    feat: add home page information when signed in

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSigninCheck } from 'reactfire';
 import styled from 'styled-components';
+import { List, Card } from 'antd';
 import { LoginToViewButton } from '~/components';
 
 const HomePageStyled = styled.div`
@@ -19,7 +22,55 @@ const TitleStyled = styled.h1`
 
 const Subtitle = styled.h2``;
 
+const CardShadowStyled = styled.div`
+  box-shadow: 0 6px 8px rgba(0, 0, 0, 0.1);
+`;
+
+const data = [
+  {
+    title: 'Explore',
+    content: 'View all available rooms now',
+    link: '/explore',
+  },
+  {
+    title: 'Your rooms',
+    content: 'The rooms you joined or created',
+    link: '/your-rooms',
+  },
+  {
+    title: 'Profile',
+    content: 'View your profile information',
+    link: '/profile',
+  },
+];
+
 function HomePage() {
+  const { status: signedInStatus, data: signedInData } = useSigninCheck();
+
+  if (signedInStatus === 'success' && signedInData.signedIn) {
+    return (
+      <HomePageStyled>
+        <h1>Home</h1>
+        <List
+          grid={{
+            gutter: 16,
+            column: 3,
+          }}
+          dataSource={data}
+          renderItem={(item) => (
+            <List.Item>
+              <Link to={item.link}>
+                <CardShadowStyled>
+                  <Card title={item.title}>{item.content}</Card>
+                </CardShadowStyled>
+              </Link>
+            </List.Item>
+          )}
+        />
+      </HomePageStyled>
+    );
+  }
+
   return (
     <HomePageStyled>
       <WelcomeStyled>
