d128dbfa37c22a17a4590fda811d9426dafded06
Lee TB <tblyduc2412@gmail.com>
2022-09-17 20:01:11 +0700

    feat: add editable room title for ownner

--- src/features/room/Room/Room.jsx
+++ src/features/room/Room/Room.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { NavLink, Outlet, useLocation } from 'react-router-dom';
-import { Typography, Menu } from 'antd';
+import { Typography, Menu, message } from 'antd';
 import styled from 'styled-components';
 
-import { doc } from 'firebase/firestore';
-import { useFirestore, useFirestoreDocData } from 'reactfire';
+import { doc, updateDoc } from 'firebase/firestore';
+import { useFirestore, useFirestoreDocData, useUser } from 'reactfire';
 
 const { Title } = Typography;
 
@@ -25,14 +25,31 @@ function Room() {
   const { status: roomStatus, data: roomData } = useFirestoreDocData(
     doc(firestore, `rooms/${roomId}`)
   );
+  const [roomName, setRoomName] = useState('Unknow room name');
+  const { status: userStatus, data: userData } = useUser();
+
+  useEffect(() => {
+    setRoomName(roomData?.roomName);
+  }, [roomData?.roomName]);
+
+  const saveRoomName = (value) => {
+    updateDoc(doc(firestore, `rooms/${roomId}`), {
+      roomName: value,
+    })
+      .then(() => {
+        message.success('update room name success');
+        setRoomName(value);
+      })
+      .catch(message.error('update room name error'));
+  };
 
   return (
     <>
-      <Title>
-        {roomStatus === 'success' && roomData
-          ? roomData?.roomName
-          : 'Unknow room name'}
-      </Title>
+      {userStatus === 'success' && userData?.uid === roomData?.owner?.uid ? (
+        <Title editable={{ onChange: saveRoomName }}>{roomName}</Title>
+      ) : (
+        <Title>{roomName}</Title>
+      )}
       <NavbarStyled>
         <Menu
           mode="horizontal"
